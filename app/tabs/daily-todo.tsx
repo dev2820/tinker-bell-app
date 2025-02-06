deb15d4de4eadf7ea21a5306a44120d7ae36163a
dev2820 <second28200@naver.com>
2025-02-07 00:53:58 +0900

    fix: url 맞춤

--- app/tabs/daily-todo.tsx
+++ app/tabs/daily-todo.tsx
@@ -1,6 +1,6 @@
 import WebviewContainer from "@/components/webview/WebviewContainer";
 
 export default function DailyTodoScreen() {
-  return <WebviewContainer url="https://ticketbell.store" />;
+  return <WebviewContainer url="https://www.ticketbell.store" />;
 }
 DailyTodoScreen.name = "DailyTodoScreen";
